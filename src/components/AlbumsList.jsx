91d8d59fa465a0c35e5dae6809afad59f6298712
Eamll <alexis.0804yes@gmail.com>
2024-01-07 15:30:50 -0400

    feature: show albums names from api

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,15 +1,37 @@
 import PropTypes from "prop-types"
 import { useGetAlbumsQuery } from "../store"
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+
 
 const AlbumsList = ({ user }) => {
     const { data, error, isLoading } = useGetAlbumsQuery(user);
+    var content;
 
-    console.log(data, error, isLoading);
-    return (
+    if (isLoading) {
+        content = <Skeleton times={3} />
+    } else
+
+        if (error) {
+            content = <div>Error: {error}</div>
+        } else {
+            content = data.map((album) => {
+                const header = <div>{album.name}</div>;
+                return <ExpandablePanel key={album.id} header={header}>
+                    List of photos in the album
+                </ExpandablePanel>
+            }
+            );
+        }
+
+    return <div>
         <div>
-            Albums by {user.name}
+            Album for {user.name}
         </div>
-    )
+        <div>{content}</div>
+    </div>;
+
 }
 
 AlbumsList.propTypes = {
